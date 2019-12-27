da4c48cf5fb3be441699f37783b8b9bc460fc483
Falabar1995 <59267989+Falabar1995@users.noreply.github.com>
2019-12-27 07:10:00 +0700

    Update pythonweb.py

--- pythonweb.py
+++ pythonweb.py
@@ -20,9 +20,9 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('yQh1VyVtqSvQck/1blc8QQ7sSxdIPNI9+CYB78XF9dUP0Ho2lGJ02rtnlaqe6aFlUeyVCszB6P6dy3GM5VgoY39OWbwmPCcljSOBcBbke6lc5ePxoaWp9pIRiC5M/hC2w/m25KnZ4MRvEOliFccGhAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('2ba452b79e30fcf854be2b49785a7bd5')
 
-APPID="LineBotRpi"
-KEY = "bjuWQftBs1hMHSw"
-SECRET = "EUUdgsgDClUdqdzIDGnwjZ752"
+APPID="chatbotpi"
+KEY = "fPWYc5zjuSyNCvQ"
+SECRET = "TTijCZNFhCh6l3XEQwE2TlCNO"
 Topic = "/LED_Control"
 
 url = 'https://api.netpie.io/topic/' + str(APPID) + str(Topic)
